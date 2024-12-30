4fb39f307f94331dd21b3c1c1ce0e06ae8b62a5e
MaTi <matiasnicolaaas@gmail.com>
2024-12-30 22:22:57 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["un perro", "un ladron", "mi mama", "el chofer"];
+  let action = ["se comio", "me robo", "mojo", "se llevo"];
+  let what = ["mi proyecto", "mi laptop "];
+  let when = [
+    "en el bus",
+    "en la cocina",
+    "en el centro",
+    "en la piscina",
+    "en el avion",
+    "en la sala"
+  ];
+
+  console.log(who[Math.floor(Math.random() * who.length)]);
+  console.log(action[Math.floor(Math.random() * action.length)]);
+  console.log(what[Math.floor(Math.random() * what.length)]);
+  console.log(when[Math.floor(Math.random() * when.length)]);
+
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)] +
+    ".";
+  console.log(excuse);
+  document.getElementById("excuse").innerHTML = excuse;
 };
+document.getElementById("excuse");
